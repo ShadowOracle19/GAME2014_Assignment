11408796a23c24c064354da6faca2a07de0e6dfb
ShadowOracle19 <lucas.g@hotmail.ca>
2020-10-04 16:48:03 -0400

    first commit

---
+++ Assets/_Scripts/startButtonInstruction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startButtonInstruction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnStartButtonPressed()
+    {
+        Debug.Log("Instruction");
+        SceneManager.LoadScene("Instruction");
+    }
+}
