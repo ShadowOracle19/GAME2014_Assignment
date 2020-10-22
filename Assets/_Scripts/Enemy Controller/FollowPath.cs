80287f87ea3f09e30061f11fde140d40b5808a00
ShadowOracle19 <lucas.g@hotmail.ca>
2020-10-22 16:02:08 -0400

    Added enemy sprites and enemy movement

---
+++ Assets/_Scripts/Enemy Controller/FollowPath.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    //Array of waypoints to walk from one to the next one 
+    public Transform[] waypoints;
+
+    //walk speed that can be set 
+    public float moveSpeed = 2f;
+
+    //Index of current waypoint from which enemy walks
+    private int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        //if enemy didnt reach last waypoint it can move
+        if(waypointIndex <= waypoints.Length - 1)
+        {
+            //move enemy from current waypoint to the next one
+            transform.position = Vector2.MoveTowards(transform.position,
+                waypoints[waypointIndex].transform.position,
+                moveSpeed * Time.deltaTime);
+
+            //if enemy reaches waypoint increase index by 1
+            if(transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+    }
+}
